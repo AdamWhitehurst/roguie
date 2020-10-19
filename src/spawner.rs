d09c40d03e67790c6df4461660a37de96f886196
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-10-19 13:53:15 -0700

    🩹spawner errors

--- src/spawner.rs
+++ src/spawner.rs
@@ -89,6 +89,7 @@ fn monster<S: ToString>(ecs: &mut World, x: i32, y: i32, glyph: rltk::FontCharTy
 /// Fills a room with stuff!
 pub fn spawn_room(ecs: &mut World, room: &Rect) {
     let mut monster_spawn_points: Vec<usize> = Vec::new();
+    let mut item_spawn_points: Vec<usize> = Vec::new();
 
     // Scope to keep the borrow checker happy
     {
@@ -114,7 +115,7 @@ pub fn spawn_room(ecs: &mut World, room: &Rect) {
             while !added {
                 let x = (room.x1 + rng.roll_dice(1, i32::abs(room.x2 - room.x1))) as usize;
                 let y = (room.y1 + rng.roll_dice(1, i32::abs(room.y2 - room.y1))) as usize;
-                let idx = (y * MAPWIDTH) + x;
+                let idx = (y * MAP_WIDTH) + x;
                 if !item_spawn_points.contains(&idx) {
                     item_spawn_points.push(idx);
                     added = true;
@@ -130,8 +131,8 @@ pub fn spawn_room(ecs: &mut World, room: &Rect) {
     }
 
     for idx in item_spawn_points.iter() {
-        let x = *idx % MAPWIDTH;
-        let y = *idx / MAPWIDTH;
+        let x = *idx % MAP_WIDTH;
+        let y = *idx / MAP_WIDTH;
         health_potion(ecs, x as i32, y as i32);
     }
 }
