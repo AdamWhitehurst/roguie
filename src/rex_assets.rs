3b9e79868e6c353faf2a640800c2f428eb88970a
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-11-29 16:12:20 -0800

    FEAT: 🖼 Main menu background art!

--- src/rex_assets.rs
+++ src/rex_assets.rs
@@ -1,6 +1,6 @@
 use rltk::rex::XpFile;
 
-rltk::embedded_resource!(SMALL_DUNGEON, "../resources/SmallDungeon_80x50.xp");
+rltk::embedded_resource!(MENU_BG, "../resources/menu_bg.xp");
 
 pub struct RexAssets {
     pub menu: XpFile,
@@ -9,10 +9,10 @@ pub struct RexAssets {
 impl RexAssets {
     #[allow(clippy::new_without_default)]
     pub fn new() -> RexAssets {
-        rltk::link_resource!(SMALL_DUNGEON, "../resources/SmallDungeon_80x50.xp");
+        rltk::link_resource!(MENU_BG, "../resources/menu_bg.xp");
 
         RexAssets {
-            menu: XpFile::from_resource("../resources/SmallDungeon_80x50.xp").unwrap(),
+            menu: XpFile::from_resource("../resources/menu_bg.xp").unwrap(),
         }
     }
 }
