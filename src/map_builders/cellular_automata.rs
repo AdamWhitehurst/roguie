7a04381e686502df3c56ab233f5dd9cef2ebe6f8
Adam Whitehurst <Adam.Whitehurst@live.com>
2020-12-07 14:14:18 -0800

    FEAT: ◼◾▪ Cellular Automata Map

---
+++ src/map_builders/cellular_automata.rs
@@ -0,0 +1,206 @@
+use super::common::*;
+use super::{
+    spawner, Map, MapBuilder, Position, Rect, TileType, MIN_ROOM_SIZE, SHOW_MAPGEN_VISUALIZER,
+};
+use rltk::RandomNumberGenerator;
+use specs::prelude::*;
+
+pub struct CellularAutomataBuilder {
+    map: Map,
+    starting_position: Position,
+    depth: i32,
+    history: Vec<Map>,
+}
+
+impl MapBuilder for CellularAutomataBuilder {
+    fn get_map(&self) -> Map {
+        self.map.clone()
+    }
+
+    fn get_starting_position(&self) -> Position {
+        self.starting_position.clone()
+    }
+
+    fn get_snapshot_history(&self) -> Vec<Map> {
+        self.history.clone()
+    }
+
+    fn build_map(&mut self) {
+        self.build();
+    }
+
+    fn spawn_entities(&mut self, ecs: &mut World) {
+        // We need to rewrite this, too.
+    }
+
+    fn take_snapshot(&mut self) {
+        if SHOW_MAPGEN_VISUALIZER {
+            let mut snapshot = self.map.clone();
+            for v in snapshot.revealed_tiles.iter_mut() {
+                *v = true;
+            }
+            self.history.push(snapshot);
+        }
+    }
+}
+
+impl CellularAutomataBuilder {
+    pub fn new(new_depth: i32) -> CellularAutomataBuilder {
+        CellularAutomataBuilder {
+            map: Map::new(new_depth),
+            starting_position: Position { x: 0, y: 0 },
+            depth: new_depth,
+            history: Vec::new(),
+        }
+    }
+
+    fn build(&mut self) {
+        let mut rng = RandomNumberGenerator::new();
+
+        // Completely randomize the map, setting 55% of it to be floor.
+        for y in 1..self.map.height - 1 {
+            for x in 1..self.map.width - 1 {
+                let roll = rng.roll_dice(1, 100);
+                let idx = self.map.xy_idx(x, y);
+                if roll > 55 {
+                    self.map.tiles[idx] = TileType::Floor
+                } else {
+                    self.map.tiles[idx] = TileType::Wall
+                }
+            }
+        }
+        self.take_snapshot();
+
+        // Iteratively apply cellular automata rules
+        for _i in 0..15 {
+            let mut newtiles = self.map.tiles.clone();
+
+            // For every tile
+            for y in 1..self.map.height - 1 {
+                for x in 1..self.map.width - 1 {
+                    let idx = self.map.xy_idx(x, y);
+                    let neighbors = self.dangerously_count_neighbors(idx);
+
+                    // Crowd walls together or add a Wall in very open spaces
+                    if neighbors > 4 || neighbors == 0 {
+                        newtiles[idx] = TileType::Wall;
+                    } else {
+                        newtiles[idx] = TileType::Floor;
+                    }
+                }
+            }
+
+            self.map.tiles = newtiles.clone();
+            self.take_snapshot();
+        }
+
+        // Clear out lonely pillars
+        let mut newtiles = self.map.tiles.clone();
+        for y in 1..self.map.height - 1 {
+            for x in 1..self.map.width - 1 {
+                let idx = self.map.xy_idx(x, y);
+                let neighbors = self.dangerously_count_neighbors(idx);
+
+                if neighbors < 2 {
+                    newtiles[idx] = TileType::Floor;
+                }
+            }
+        }
+        self.map.tiles = newtiles.clone();
+        self.take_snapshot();
+
+        // Find a starting point
+        // Start at the middle and
+        self.starting_position = Position {
+            x: self.map.width / 2,
+            y: self.map.height / 2,
+        };
+        let mut start_idx = self
+            .map
+            .xy_idx(self.starting_position.x, self.starting_position.y);
+
+        //  Walk left until we find an open tile
+        while self.map.tiles[start_idx] != TileType::Floor {
+            self.starting_position.x -= 1;
+            start_idx = self
+                .map
+                .xy_idx(self.starting_position.x, self.starting_position.y);
+        }
+
+        // Find all tiles we can reach from the starting point
+        let map_starts: Vec<usize> = vec![start_idx];
+        // http://www.roguebasin.com/index.php?title=The_Incredible_Power_of_Dijkstra_Maps
+        // Get a map of distance values from map_starts
+        let dijkstra_map = rltk::DijkstraMap::new(
+            self.map.width,
+            self.map.height,
+            &map_starts,
+            &self.map,
+            200.0,
+        );
+
+        // (Index of exit tile, distance from start)
+        let mut exit_tile = (0, 0.0f32);
+        for (i, tile) in self.map.tiles.iter_mut().enumerate() {
+            if *tile == TileType::Floor {
+                let distance_to_start = dijkstra_map.map[i];
+                // We can't get to this tile - so we'll make it a wall
+                if distance_to_start == std::f32::MAX {
+                    *tile = TileType::Wall;
+                } else {
+                    // If it is further away than our current exit candidate, move the exit
+                    if distance_to_start > exit_tile.1 {
+                        exit_tile.0 = i;
+                        exit_tile.1 = distance_to_start;
+                    }
+                }
+            }
+        }
+        self.take_snapshot();
+
+        self.map.tiles[exit_tile.0] = TileType::DownStairs;
+        self.take_snapshot();
+    }
+
+    /// Counts how many Wall neighbors passed `idx` has (both orthogonal and
+    /// diagonal)
+    ///
+    /// !! WARNING: Does not check bounds !!
+    fn dangerously_count_neighbors(&self, idx: usize) -> usize {
+        let mut neighbors = 0;
+        // x - 1
+        if self.map.tiles[idx - 1] == TileType::Wall {
+            neighbors += 1;
+        }
+        // x + 1
+        if self.map.tiles[idx + 1] == TileType::Wall {
+            neighbors += 1;
+        }
+        // y - 1
+        if self.map.tiles[idx - self.map.width as usize] == TileType::Wall {
+            neighbors += 1;
+        }
+        // y + 1
+        if self.map.tiles[idx + self.map.width as usize] == TileType::Wall {
+            neighbors += 1;
+        }
+        // x - 1, y - 1
+        if self.map.tiles[idx - (self.map.width as usize - 1)] == TileType::Wall {
+            neighbors += 1;
+        }
+        // x + 1, y - 1
+        if self.map.tiles[idx - (self.map.width as usize + 1)] == TileType::Wall {
+            neighbors += 1;
+        }
+        // x - 1, y + 1
+        if self.map.tiles[idx + (self.map.width as usize - 1)] == TileType::Wall {
+            neighbors += 1;
+        }
+        // x + 1, y + 1
+        if self.map.tiles[idx + (self.map.width as usize + 1)] == TileType::Wall {
+            neighbors += 1;
+        }
+
+        neighbors
+    }
+}
--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -2,12 +2,14 @@ use super::*;
 
 mod bsp_dungeon;
 mod bsp_interior;
+mod cellular_automata;
 mod common;
 mod simple_map;
 
 // use common::*;
 use bsp_dungeon::*;
 use bsp_interior::*;
+use cellular_automata::*;
 use simple_map::*;
 
 const MIN_ROOM_SIZE: i32 = 8;
@@ -23,10 +25,11 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 3);
+    let builder = rng.roll_dice(1, 4);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
+        3 => Box::new(CellularAutomataBuilder::new(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 }
